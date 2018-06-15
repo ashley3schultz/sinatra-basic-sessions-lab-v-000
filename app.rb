f76e5ec772d6d68c264b816613de9f3573248576
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 00:45:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/hey' do 
+    @session = session
+  end
+
   get '/' do
     erb :index
   end
 
   post '/' do
     params[:item]
-    configure do
-      enable :sessions
-      set :session_secret, "secret"
-    end
+    
   end
 end
